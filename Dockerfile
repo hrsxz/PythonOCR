5913a81db2dea3835157f7c0a9f9754fd1475b51
Zhou <hrsxz@hotmail.com>
2024-03-25 03:36:03 +0800

    Add

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,8 @@ ARG DEBIAN_FRONTEND=noninteractive
 
 RUN apt-get update && apt-get install -y python3-opencv libglib2.0-0 libsm6 libxext6 libxrender-dev && rm -rf /var/lib/apt/lists/*
 
-RUN  pip install -U pip && pip install onnxruntime && pip install cnocr[serve] --index-url https://mirrors.aliyun.com/pypi/simple
+# RUN  pip install -U pip && pip install onnxruntime && pip install cnocr[serve] --index-url https://mirrors.aliyun.com/pypi/simple
+RUN pip install -U pip && pip install onnxruntime && pip install cnocr[serve]
 
 CMD ["cnocr", "serve", "-H", "0.0.0.0", "-p", "8501"]
 
--- scripts/check_label_cn.py
+++ scripts/check_label_cn.py
@@ -10,7 +10,15 @@ BAD_CHARS = [5751, 5539, 5536, 5535, 5464, 4105]
 
 
 def main():
-    charset_fp = os.path.join(data_dir(), 'models', 'label_cn.txt')
+    # charset_fp = os.path.join(data_dir(), 'models', 'label_cn.txt')
+    # 当前脚本的目录
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    # 构造 label_cn.txt 的路径
+    label_file_path = os.path.join(script_dir, '..', 'cnocr', 'label_cn.txt')
+    # 确保路径是正确的
+    label_file_path = os.path.normpath(label_file_path)
+    print("label_cn.txt 文件的位置:", label_file_path)
+    charset_fp = os.path.join(label_file_path)
     alphabet, inv_alph_dict = read_charset(charset_fp)
     for idx in BAD_CHARS:
         print('idx: {}, char: {}'.format(idx, alphabet[idx]))
